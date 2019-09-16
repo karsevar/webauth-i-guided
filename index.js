50e2f9bc18182696a5c514c35feb489a910fdc4a
masonkarsevar <masonkarsevar@gmail.com>
2019-09-16 10:42:07 -0700

    finished restricted middleware script

--- index.js
+++ index.js
@@ -38,19 +38,22 @@ server.post('/api/login', (req, res) => {
     Users.findBy({ username })
       .first()
       .then(user => {
+        // check password
         if (user && bcrypt.compareSync(password, user.password)) {
           res.status(200).json({ message: `Welcome ${user.username}!` });
         } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
+          res.status(401).json({ message: 'You can not pass' });
         }
       })
       .catch(error => {
         res.status(500).json(error);
       });
+  } else {
+    res.status(401).json({message: "you need to pass a username or password"})
   }
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -64,6 +67,26 @@ server.get('/hash', (req, res) => {
   const hash = bcrypt.hashSync(name, 12);
   res.send(`The hash for ${name} is ${hash}`)
 });
+
+// middleware:
+function restricted (req, res, next){
+  const {username, password} = req.headers;
+
+  if (username && password) {
+    Users.findBy({username})
+      .first() 
+      .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({message: "invalidate login"})
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      })
+  }
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
